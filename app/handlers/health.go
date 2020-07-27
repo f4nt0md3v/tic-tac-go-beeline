540e9dedbb76f61638b416b85bbeb9a49d8bcdbb
f4nt0md3v <yzhanabayev@dar.kz>
2020-07-27 17:37:11 +0600

    Refactoring

--- app/handlers/health.go
+++ app/handlers/health.go
@@ -6,6 +6,6 @@ import (
 	"github.com/f4nt0md3v/tic-tac-go-beeline/app/pkg/netx/httpx"
 )
 
-func HealthHandler(w http.ResponseWriter, r *http.Request) {
+func HealthHandler(w http.ResponseWriter, _ *http.Request) {
 	httpx.WriteJson(w, http.StatusOK, http.StatusText(http.StatusOK))
 }
